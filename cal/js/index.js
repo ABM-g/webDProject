6663d7ae18db68401739cd2bbe0d90efafdcd8dd
ABM-g <alibassam20@gmail.com>
2022-01-06 23:19:13 +0300

    we

--- cal/js/index.js
+++ cal/js/index.js
@@ -24,6 +24,7 @@ function load() {
                     c.play();
                     break;
                 case '=':
+					e.play();
                     var lastChar = input[input.length - 1];
                     if(operators.indexOf(lastChar) > -1 || lastChar == '.')
                         input = input.replace(/.$/, '');
@@ -33,7 +34,6 @@ function load() {
                         inputScreen.innerHTML = eval(input);
                     }
                     decimalAdded = false;
-                    e.play();
                     break;
                 case '.':
                     if(!decimalAdded) {
@@ -46,18 +46,18 @@ function load() {
                 case '-':
                 case 'x':
                 case '÷':
-                    // Last char of string
+                 
                     var lastChar = input[input.length - 1];
 
-                    // Only add operator if input is not empty and there is no operator at the last
+                    
                     if(input != '' && operators.indexOf(lastChar) == -1)
                         inputScreen.innerHTML += btnValue;
 
-                    // Allows minus if the string is empty. The first number could be under zero
+                    
                     else if(input == '' && btnValue == '-')
                         inputScreen.innerHTML += btnValue;
 
-                    // Allows to represent the last operation
+                    
                     if(operators.indexOf(lastChar) > -1 && input.length > 1) {
                         inputScreen.innerHTML = input.replace(/.$/, btnValue);
                     }
